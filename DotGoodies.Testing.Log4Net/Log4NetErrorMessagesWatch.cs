9bea57f5577a10bab4811bf25247396c5ffc761c
Igor Fedurkin <igor.fedurkin@pds.nl>
2015-11-21 17:41:15 +0100

    Code base for log4net test aspects added

---
+++ DotGoodies.Testing.Log4Net/Log4NetErrorMessagesWatch.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+
+namespace DotGoodies.Testing.Log4Net
+{
+    public class Log4NetErrorMessagesWatch : AppenderSkeleton
+    {
+        private static ConcurrentBag<Tuple<string, Exception>> _exceptions;
+        private static ConcurrentBag<string> _messages;
+        private static readonly string SelfTestUniqueMessage = $"Self test {Guid.NewGuid()}.";
+
+        private static bool _selfTestPassed = false;
+
+        internal static void SetUp()
+        {
+            _exceptions = new ConcurrentBag<Tuple<string, Exception>>();
+            _messages = new ConcurrentBag<string>();
+
+            var testLogger = LogManager.GetLogger(typeof(Log4NetErrorMessagesWatch));
+
+            testLogger.ErrorFormat(SelfTestUniqueMessage);
+
+            if (!_selfTestPassed)
+                throw new InvalidOperationException("Log4NetErrorMessagesWatch is not properly configured.");
+        }
+
+        internal static void TearDown()
+        {
+            _selfTestPassed = false;
+
+            _exceptions = new ConcurrentBag<Tuple<string, Exception>>();
+            _messages = new ConcurrentBag<string>();
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (loggingEvent.RenderedMessage.Equals(SelfTestUniqueMessage))
+            {
+                _selfTestPassed = true;
+                return;
+            }
+
+            if (loggingEvent.Level != Level.Error && loggingEvent.Level != Level.Fatal)
+                return;
+
+            if (loggingEvent.ExceptionObject != null)
+            {
+                _exceptions.Add(Tuple.Create(loggingEvent.RenderedMessage, loggingEvent.ExceptionObject));
+                return;
+            }
+
+            _messages.Add(loggingEvent.RenderedMessage);
+        }
+
+        public void PrintExceptions()
+        {
+            if (_exceptions.Count == 0 && _messages.Count == 0)
+                return;
+
+            if (_exceptions.Count > 0)
+                Debug.WriteLine("Exceptions:");
+
+            foreach (var exception in _exceptions)
+            {
+                Debug.WriteLine("Message: {0}. Exception: {1}", exception.Item1, exception.Item2);
+            }
+
+            if (_messages.Count > 0)
+                Debug.WriteLine("Messages:");
+
+            foreach (var message in _messages)
+            {
+                Debug.WriteLine(message);
+            }
+        }
+
+        public bool HasExceptions => _exceptions.Count > 0 || _messages.Count > 0;
+    }
+}
---
+++ DotGoodies.Testing.Log4Net/LoggingTestAspect.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using log4net.Config;
+
+namespace DotGoodies.Testing.Log4Net
+{
+    public sealed class LoggingTestAspect
+    {
+        private readonly bool _enableErrorWatch;
+        private readonly string _log4NetConfigFileName;
+
+        public LoggingTestAspect(bool enableErrorWatch = false, string log4NetConfigFileName = "log4net.config")
+        {
+            _enableErrorWatch = enableErrorWatch;
+            _log4NetConfigFileName = log4NetConfigFileName;
+        }
+
+        public void SetUp()
+        {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(_log4NetConfigFileName));
+
+            if (_enableErrorWatch)
+            {
+                Log4NetErrorMessagesWatch.SetUp();
+            }
+        }
+
+        public Log4NetErrorMessagesWatch ErrorWatch => new Log4NetErrorMessagesWatch();
+
+        public void TearDown()
+        {
+            if (_enableErrorWatch)
+            {
+                Log4NetErrorMessagesWatch.TearDown();
+            }
+        }
+    }
+}
