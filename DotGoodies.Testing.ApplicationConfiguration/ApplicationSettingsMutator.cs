7765dac3880410c3380c6bb9c50850bc3b30eeca
Igor Fedurkin <igor.fedurkin@pds.nl>
2015-11-21 17:57:46 +0100

    Application settings test aspect added

---
+++ DotGoodies.Testing.ApplicationConfiguration/ApplicationSettingsMutator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Linq.Expressions;
+
+namespace DotGoodies.Testing.ApplicationConfiguration
+{
+    public sealed class ApplicationSettingsMutator<TSettings> 
+        where TSettings : ApplicationSettingsBase
+    {
+        private readonly TSettings _settings;
+
+        public ApplicationSettingsMutator(TSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public ApplicationSettingsMutator<TSettings> Override<TValue>(Expression<Func<TSettings, TValue>> property, TValue value)
+        {
+            if (!(property.Body is MemberExpression)) 
+                throw new InvalidOperationException("Unable to figure out property name.");
+
+            _settings.Reload();
+
+            var member = property.Body as MemberExpression;
+
+            var _ = _settings[member.Member.Name];
+
+            _settings.PropertyValues[member.Member.Name].PropertyValue = value;
+
+            return this;
+        }
+
+        public void Reload()
+        {
+            _settings.Reset();
+            _settings.Reload();
+        }
+    }
+}
